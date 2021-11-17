49237847098b73cb72565e0ab723626360796afd
SepehrRasouli <sepehrrs06@gmail.com>
2021-11-17 14:56:18 +0330

    Added some new changes, Will edit the code very soon.

--- Tasks/app.py
+++ Tasks/app.py
@@ -6,10 +6,60 @@ import argparse as arg
 -d For date
 -du For duration * in seconds *
 -c For comment
+-rl For reminder list
+-tl For task list
+-dr To delete a reminder
 """
 
 def reminder():
-    pass
+    #     ┌─────────────────┐
+    #     │                 │
+    #     │      Start      │
+    #     │                 │
+    #     └────────┬────────┘
+    #              │
+    #              │
+    #              │
+    #              │
+    #              │
+    #              │
+    #              │
+    #              ▼
+    #      ┌───────────────┐
+    #      │ Is there any  │     Yes
+    #      │ Active        ├──────────────────┐
+    #      │ Reminders?    │                  ▼
+    #      └───────┬───────┘             ┌──────────────┐
+    #              │                     │Import them   │
+    #              │                     │And run new   │
+    #           No │                     │Threads For   │
+    #              │                     │Each reminder.│
+    #              │                     │              │
+    #              ▼                     └──────────────┘
+    #   ┌───────────────────────┐
+    #   │  Is the user          │
+    #   │  Requesting to add a  │
+    #   │  new reminder?        ├─────┐
+    #   │                       │     │
+    #   │                       │     │
+    #   └─────┬─────────────────┘     │
+    #         │                       │
+    #         │                       │
+    #         │                       │
+    #         │                       ▼
+    #         │                       │
+    # ┌───────┴─────┐   ┌─────────────┴─────┐
+    # │             │   │      Add the new  │
+    # │     End     │   │ Date and time Into│
+    # └─────────────┘   │ database.txt      │
+    #                   │ And run a new     │
+    #                   │ Thread for the    │
+    #                   │ Reminder          │
+    #                   └───────────────────┘
+    print("Checking for active Reminders...")
+    with open("database.txt", "r") as file:
+        for element in file:
+            pass
 
 parser = arg.ArgumentParser(description="Simple app to record tasks and their times , with notifications and reminders")
 parser.add_argument("-r", "--reminder", help="Reminder for task", action="store_true")
