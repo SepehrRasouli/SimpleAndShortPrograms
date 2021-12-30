9a00d115f4dd23e56410c4e19408adc0ea502050
SepehrRasouli <sepehrrs06@gmail.com>
2021-12-30 22:01:31 +0330

    added electrons configuration class

--- Electrons Configuration calculator/app.py
+++ Electrons Configuration calculator/app.py
@@ -1,5 +1,7 @@
 import colorama
 
+class Electrons_Configurations:
+    pass
 
 def main():
     print(f'{colorama.Fore.CYAN}Elements Details{colorama.Fore.RESET}')
@@ -7,6 +9,7 @@ def main():
     atomic_number = int(input("> ")) 
     print(f'{colorama.Fore.CYAN}Please Enter The element\'s Charge *Enter Zero If The Element Dosen\'t Have Any Charge* => {colorama.Fore.RESET}')
     charge = int(input("> "))
+    electrons_configs: list = Electrons_Configurations() #TODO : Fix This.
 
 if __name__ == '__main__':
     main()
