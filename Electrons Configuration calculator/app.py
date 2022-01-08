7f6fc641b398f45793c9895ca4e9518ad93284b4
SepehrRasouli <sepehrrs06@gmail.com>
2022-01-08 10:07:36 +0330

    Did some modifications and fixed __init__ of electrons configuration class

--- Electrons Configuration calculator/app.py
+++ Electrons Configuration calculator/app.py
@@ -2,12 +2,24 @@ import colorama
 from colorama import Fore, Back, Style
 class Electrons_Configurations:
     #TODO: Complete This Class
-    def __init__(self,atomic_number,charge):
-        self.atomic_number = atomic_number
-        self.charge = charge
+    def __init__(self,atomic_number:int,charge:int=0):
+        self.atomic_number:int = atomic_number
+        self.charge:int = charge
+        self.aufbau_principle = {
+                "1s":"","2s":"","2p":"","3s":"","3p":"",
+                "4s":"","3d":"","4p":"","5s":"","4d":"","5p":"",
+                "6s":"","4f":"","5d":"","6p":"","7s":"","5f":"",
+                "6d":"","7p":"",
+                }
+        self.noble_gas_list:dict = {"He":[1,2],"Ne":[2,10],"Ar":[3,18],"Kr":[4,36],"Xe":[5,54],"Rn":[6,86]}
+        self.arranged:list = ["1s","2s","2p","3s","3p","3d","4s","4p","4d","4f","5s","5p","5d","5f","6s","6p","6d","7s","7p"]
 
     def main(self):
-        pass
+        '''This is the main function of the Electrons_configuration class.
+        Returns: 
+        A list of the both complete and summarized configuration of the element and the filled aufbau principle dictionary.
+        '''
+
 
     def complete_config(self):
         pass
@@ -24,8 +36,9 @@ def main():
     atomic_number = int(input("> ")) 
     print(f'{colorama.Fore.CYAN}Please Enter The element\'s Charge *Enter Zero If The Element Dosen\'t Have Any Charge* => {colorama.Fore.RESET}')
     charge = int(input("> "))
-    Electrons_Configuration = Electrons_Configurations(atomic_number,charge)
-    configs: list = Electrons_Configuration.main()
+    Electrons_Configuration = Electrons_Configurations(int(atomic_number),int(charge))
+    configs: list = Electrons_Configuration.main() # This will also contain a list with the Aufbau principle of the element's electron's configuration
+
 
 
 if __name__ == '__main__':
