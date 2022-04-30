e068147510cf1911f469a7e28ec207cf49ed3e06
SepehrRasouli <sepehrrs06@gmail.com>
2022-04-30 15:55:51 +0430

    added classes

--- timer.py
+++ timer.py
@@ -1,4 +1,15 @@
 # Simple alternative timer to Microsoft's Alarms & Clock. 
 # I'm making this app since I couldn't find any FOSS alternative to 'Alarms & Clock' app.
 
+class cli:
+    pass
+
+class database:
+    pass
+
+class show:
+    pass
+
+class timer:
+    pass
 
