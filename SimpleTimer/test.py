b495a6ca898bc6b59488487c0d72eba1cab5dbcf
SepehrRasouli <sepehrrs06@gmail.com>
2022-05-02 20:20:13 +0430

    added remove database test and create database test.

--- SimpleTimer/test.py
+++ SimpleTimer/test.py
@@ -1,3 +1,24 @@
 import timer
+import os
+from config import DATABASE_NAME
 database = timer.database()
-print(database)
+def test_createDatabase():
+    # Test with override set to False
+    database.createDatabase(override=False)
+    assert os.path.isfile(DATABASE_NAME)
+
+    # Test with override set to True
+    database.createDatabase(override=False)
+    assert os.path.isfile(DATABASE_NAME)
+
+    os.remove(DATABASE_NAME)
+
+def test_removeDatabase():
+    database.createDatabase()
+    database.removeDatabase()
+    assert not os.path.isfile(DATABASE_NAME)
+
+def test_dumpDataToDatabase():
+    database.createDatabase()
+    database.dumpDataToDatabase([15])
+
