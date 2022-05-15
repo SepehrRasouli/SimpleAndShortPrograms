bec34e7a43847c4b00a41f29a9247776aff1ab14
SepehrRasouli <sepehrrs06@gmail.com>
2022-05-15 12:51:03 +0430

    fixing tests.

--- SimpleTimer/test.py
+++ SimpleTimer/test.py
@@ -1,10 +1,21 @@
 import timer
 import os
+import pickle
+import random
+import string
 from config import DATABASE_NAME
 database = timer.database()
+def readPickleData(database:str) -> list:
+    '''Reads & Returns pickle data.'''
+    with open(database,'rb') as pf:
+        data = pickle.load(pf)
+    return data
+
 def test_createDatabase():
+    '''Tests createDatabse function.'''
     # Test with override set to False
-    database.createDatabase(override=False)
+    fileName = random.sample(string.ascii_letters)
+    database.createDatabase(override=False,databaseName='')
     assert os.path.isfile(DATABASE_NAME)
 
     # Test with override set to True
@@ -14,11 +25,13 @@ def test_createDatabase():
     os.remove(DATABASE_NAME)
 
 def test_removeDatabase():
+    '''Tests removedatabase function.'''
     database.createDatabase()
     database.removeDatabase()
     assert not os.path.isfile(DATABASE_NAME)
 
 def test_dumpDataToDatabase():
+    '''Tests dumpDataToDatabase function.'''
     database.createDatabase()
     database.dumpDataToDatabase([15])
-
+    data = readPickleData()
